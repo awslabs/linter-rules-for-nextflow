09aad45bd9ca2291629ea7741a635ac92ceba0d7
Mark Schreiber <mrschre@amazon.com>
2024-01-17 12:10:35 -0500

    rule for missing directives

--- healthomics-nextflow-rules/src/main/groovy/software/amazon/nextflow/rules/healthomics/MissingProcessDirectivesRule.groovy
+++ healthomics-nextflow-rules/src/main/groovy/software/amazon/nextflow/rules/healthomics/MissingProcessDirectivesRule.groovy
@@ -1,4 +1,93 @@
 package software.amazon.nextflow.rules.healthomics
 
-class MissingProcessDirectivesRule {
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+/**
+ * This rule inspects Nextflow processes to determine if recommended directives are missing. Note that if a "label"
+ * directive is present then we assume that the processes required directives are declared in a config file and are
+ * therefore not required here.
+ */
+class MissingProcessDirectivesRule extends AbstractAstVisitorRule{
+    String name = 'MissingProcessDirectivesRule'
+    int priority = 2
+    Class astVisitorClass = MissingProcessDirectivesVisitor
+}
+
+class MissingProcessDirectivesVisitor extends AbstractAstVisitor {
+    boolean hasProcessLabel = false
+    boolean declaresCpus = false
+    boolean declaresMemory = false
+    boolean declaresContainer = false
+    String processName = null
+    int nestedProcessMethodDepth = 0;
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression node) {
+        String methodName = node.getMethodAsString()
+        if (methodName == 'process') {
+            nestedProcessMethodDepth++
+        } else if (nestedProcessMethodDepth > 0){
+            nestedProcessMethodDepth++
+
+            // at depth >= 2 method calls might be process directives
+            if (nestedProcessMethodDepth >= 2) {
+                switch (methodName) {
+                    case 'cpus':
+                        declaresCpus = true
+                        break
+                    case 'memory':
+                        declaresMemory = true
+                        break
+                    case 'container':
+                        declaresContainer = true
+                        break
+                    case 'label':
+                        hasProcessLabel = true
+                        break
+                    default:
+                        //no-op
+                        break
+                }
+            }
+        }
+
+        super.visitMethodCallExpression(node)
+        exitMethodCall(node)
+    }
+
+    @Override
+    void visitConstantExpression(ConstantExpression expression){
+        if (nestedProcessMethodDepth == 2 && !processName){
+            processName = expression.value
+        }
+
+        super.visitConstantExpression(expression)
+    }
+
+    private void exitMethodCall(MethodCallExpression node) {
+        if (nestedProcessMethodDepth == 1) {
+            if(!hasProcessLabel) {
+                if (!declaresCpus) {
+                    addViolation(node, "$processName does not contain a cpus directive which is recommended for reproducibility")
+                }
+
+                if (!declaresMemory) {
+                    addViolation(node, "$processName does not contain a memory directive which is recommended for reproducibility")
+                }
+                if (!declaresContainer) {
+                    addViolation(node, "$processName does not contain a container directive which is recommended for reproducibility")
+                }
+            }
+
+            hasProcessLabel = false
+            declaresCpus = false
+            declaresMemory = false
+            processName = null
+        }
+
+        nestedProcessMethodDepth--
+    }
 }
--- healthomics-nextflow-rules/src/test/groovy/software/amazon/nextflow/rules/healthomics/MissingProcessDirectivesRuleTest.groovy
+++ healthomics-nextflow-rules/src/test/groovy/software/amazon/nextflow/rules/healthomics/MissingProcessDirectivesRuleTest.groovy
@@ -1,4 +1,80 @@
 package software.amazon.nextflow.rules.healthomics
 
-class MissingProcessDirectivesRuleTest {
+import org.codenarc.rule.AbstractRuleTestCase
+import org.junit.jupiter.api.Test
+
+class MissingProcessDirectivesRuleTest extends AbstractRuleTestCase<MissingProcessDirectivesRule> {
+    @Override
+    protected MissingProcessDirectivesRule createRule(){
+        new MissingProcessDirectivesRule()
+    }
+
+    @Test
+    void ruleProperties(){
+        assert rule.priority == 2
+        assert rule.name == "MissingProcessDirectivesRule"
+    }
+
+    @Test
+    void noDirectives_allDirectivesMissingViolation(){
+        final SOURCE =  'process MY_PROCESS {}'
+        assertViolations(SOURCE,
+            [line:1, source:SOURCE, message:'MY_PROCESS does not contain a cpus directive which is recommended for reproducibility'],
+            [line:1, source:SOURCE, message:'MY_PROCESS does not contain a memory directive which is recommended for reproducibility'],
+            [line:1, source:SOURCE, message:'MY_PROCESS does not contain a container directive which is recommended for reproducibility']
+        )
+    }
+
+    @Test
+    void onlyCpu_MemoryAndContainerMissing(){
+        final SOURCE =  'process MY_PROCESS {cpus 2}'
+        assertViolations(SOURCE,
+                [line:1, source:SOURCE, message:'MY_PROCESS does not contain a memory directive which is recommended for reproducibility'],
+                [line:1, source:SOURCE, message:'MY_PROCESS does not contain a container directive which is recommended for reproducibility']
+        )
+    }
+
+    @Test
+    void onlyCpuAndMemory_ContainerMissing(){
+        final SOURCE =
+'''process MY_PROCESS {
+        memory '2 GB'
+        cpus 2
+    }
+'''
+        assertViolations(SOURCE,
+                [line:1, source: 'process MY_PROCESS {', message:'MY_PROCESS does not contain a container directive which is recommended for reproducibility']
+        )
+    }
+
+    @Test
+    void hasAllDirectives_NoViolations(){
+        final SOURCE =
+'''process MY_PROCESS {
+        memory '2 GB'
+        cpus 2
+        container 'foo:baa'
+    }
+'''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void hasLabel_NoViolations(){
+        final SOURCE = "process MY_PROCESS {label 'BigMem'}"
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void hasLabelAndDirectives_NoViolations(){
+final SOURCE =
+'''process MY_PROCESS {
+        label 'BigMem'
+        memory '2 GB'
+        cpus 2
+        container 'foo:baa'
+    }
+'''
+        assertNoViolations(SOURCE)
+    }
 }
