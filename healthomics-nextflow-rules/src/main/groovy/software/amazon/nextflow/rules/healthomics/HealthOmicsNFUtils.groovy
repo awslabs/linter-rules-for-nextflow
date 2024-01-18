95b1e9070b5518ec38d1d66774f5bd5933e246c3
Mark Schreiber <mrschre@amazon.com>
2024-01-18 17:24:13 -0500

    list of unsupported directives

--- healthomics-nextflow-rules/src/main/groovy/software/amazon/nextflow/rules/healthomics/HealthOmicsNFUtils.groovy
+++ healthomics-nextflow-rules/src/main/groovy/software/amazon/nextflow/rules/healthomics/HealthOmicsNFUtils.groovy
@@ -1,4 +1,29 @@
 package software.amazon.nextflow.rules.healthomics
 
 class HealthOmicsNFUtils {
+    def static UNSUPPORTED_NF_PROCESS_DIRECTIVES = [
+            'afterScript',
+            'arch',
+            'beforeScript',
+            'cache',        //will be allowed when we have call caching
+            'clusterOptions',
+            'conda',
+            'containerOptions',
+            'debug',
+            'disk',
+            'echo',
+            'executor',
+            'machineType',
+            'maxForks',
+            'module',
+            'penv',
+            'pod',
+            'queue',
+            'scratch',
+            'shell',
+            'spack',
+            'stageInMode',
+            'stageOutMode',
+            'storeDir',     //might be allowed when we have call caching
+    ]
 }
