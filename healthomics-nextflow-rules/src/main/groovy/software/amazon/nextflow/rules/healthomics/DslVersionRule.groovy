a515e4dbaee9bba2450b61dddb58e3fee6978576
Mark Schreiber <mrschre@amazon.com>
2024-01-18 17:23:03 -0500

    new rules

--- healthomics-nextflow-rules/src/main/groovy/software/amazon/nextflow/rules/healthomics/DslVersionRule.groovy
+++ healthomics-nextflow-rules/src/main/groovy/software/amazon/nextflow/rules/healthomics/DslVersionRule.groovy
@@ -1,4 +1,35 @@
 package software.amazon.nextflow.rules.healthomics
 
-class DslVersionRule {
+import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.PropertyExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+class DslVersionRule extends AbstractAstVisitorRule {
+    String name = 'DslVersionRule'
+    int priority = 1
+    Class astVisitorClass = DslVersionAstVisitor
+}
+
+class DslVersionAstVisitor extends AbstractAstVisitor {
+    // todo test
+    @Override
+    void visitBinaryExpression(BinaryExpression expression) {
+        if (expression.leftExpression instanceof PropertyExpression) {
+            def propExpression = expression.leftExpression as PropertyExpression
+            if (propExpression.text == 'nextflow.enable.dsl'){
+                if (expression.rightExpression instanceof ConstantExpression) {
+                    def constExpression = expression.rightExpression as ConstantExpression
+                    if ( constExpression.value != 2){
+                        addViolation(constExpression, "HealthOmics only supports DSL version 2, found ${constExpression.value}")
+                    }
+                } else {
+                    addViolation(expression.rightExpression, "Expected a constant with a value of 2, found ${expression.rightExpression.text}")
+                }
+            }
+        }
+
+        super.visitBinaryExpression(expression)
+    }
 }
