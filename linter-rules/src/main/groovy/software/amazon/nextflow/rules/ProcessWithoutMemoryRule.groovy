aef81b6c363350dc677a983b3af27909fe4952bf
Mark Schreiber <markjschreiber@gmail.com>
2024-03-27 13:05:25 -0400

    feat: Adds a rule to recommend 'memory' directive (#17)
    * feat: Adds a rule to recommend 'memory' directive

---
+++ linter-rules/src/main/groovy/software/amazon/nextflow/rules/ProcessWithoutMemoryRule.groovy
@@ -0,0 +1,54 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.nextflow.rules
+
+import org.codehaus.groovy.ast.expr.MethodCallExpression
+import org.codenarc.rule.AbstractAstVisitor
+import org.codenarc.rule.AbstractAstVisitorRule
+
+class ProcessWithoutMemoryRule extends AbstractAstVisitorRule{
+    String name = "ProcessWithoutMemoryRule"
+    int priority = 2
+    Class astVisitorClass = ProcessWithoutMemoryAstVisitor
+}
+
+class ProcessWithoutMemoryAstVisitor extends AbstractAstVisitor {
+    boolean inProcess = false
+    boolean memoryDirectiveFound = false
+    boolean labelDirectiveFound = false
+    int depth = 0
+
+    @Override
+    void visitMethodCallExpression(MethodCallExpression call) {
+        depth++
+
+        String methodName = call.methodAsString
+
+        if (methodName == "process") {
+            inProcess = true
+        }
+
+        if (inProcess && methodName == "memory") {
+            memoryDirectiveFound = true
+        }
+
+        if (inProcess && methodName == "label") {
+            labelDirectiveFound = true
+        }
+
+        super.visitMethodCallExpression(call)
+        depth--
+
+        if(depth == 0 && inProcess && !(memoryDirectiveFound || labelDirectiveFound)) {
+            addViolation(call, "No 'label' or 'memory' directive found. This may reduce the portability of this process.")
+        }
+        if(depth == 0) {
+            inProcess = false
+            memoryDirectiveFound = false
+            labelDirectiveFound = false
+        }
+    }
+}
---
+++ linter-rules/src/test/groovy/software/amazon/nextflow/rules/ProcessWithoutMemoryTest.groovy
@@ -0,0 +1,126 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.nextflow.rules
+
+import org.codenarc.rule.AbstractRuleTestCase
+import org.junit.jupiter.api.Test
+
+class ProcessWithoutMemoryTest extends AbstractRuleTestCase<ProcessWithoutMemoryRule> {
+    protected ProcessWithoutMemoryRule createRule(){
+        return new ProcessWithoutMemoryRule()
+    }
+
+    @Test
+    void ruleProperties(){
+        assert rule.name == 'ProcessWithoutMemoryRule'
+        assert rule.priority == 2
+    }
+
+    @Test
+    void processWithMemory_NoViolation() {
+        final SOURCE = '''
+process big_job {
+  cpus 8
+  memory '2 GB'
+
+  """
+  blastp -query input_sequence -num_threads ${task.cpus}
+  """
+}
+'''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void processWithLabel_NoViolation() {
+        final SOURCE = '''
+process bigTask {
+  label 'big_mem'
+
+  """
+  task script
+  """
+}
+'''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void processWithLabelAndMemory_NoViolation() {
+        final SOURCE = '''
+process bigTask {
+  label 'big_mem'
+  memory ${foo}
+
+  """
+  task script
+  """
+}
+'''
+        assertNoViolations(SOURCE)
+    }
+
+    @Test
+    void processWithoutMemoryOrLabel_Violation() {
+        final SOURCE = '''
+process bigTask {
+  cpus ${foo}
+  """
+  task script
+  """
+}
+'''
+        assertSingleViolation(
+                SOURCE,
+                2,
+                "process bigTask {",
+                "No 'label' or 'memory' directive found. This may reduce the portability of this process."
+        )
+    }
+
+    @Test
+    void processesWithoutCpusOrLabel_Violation() {
+        final SOURCE = '''
+process bigTask {
+  cpus ${foo}
+  """
+  task script
+  """
+}
+
+process valid {
+  label "foo"
+  """
+  task script
+  """
+}
+
+process anotherTask {
+  cpus ${foo}
+  """
+  task script
+  """
+}
+
+process alsoValid {
+  cpus 12
+  memory "8 GB"
+  """
+  task script
+  """
+}
+'''
+        assertTwoViolations(
+                SOURCE,
+                2,
+                "process bigTask {",
+                "No 'label' or 'memory' directive found. This may reduce the portability of this process.",
+                16,
+                "process anotherTask {",
+                "No 'label' or 'memory' directive found. This may reduce the portability of this process."
+        )
+    }
+}
