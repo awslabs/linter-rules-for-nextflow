4cd4b4016bd5b95037719fa2d95a643bb753d87d
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-07-05 09:18:27 -0700

    Re-enable 'ext' process directive

--- linter-rules/src/main/groovy/software/amazon/nextflow/rules/utils/NFUtils.groovy
+++ linter-rules/src/main/groovy/software/amazon/nextflow/rules/utils/NFUtils.groovy
@@ -8,7 +8,7 @@ package software.amazon.nextflow.rules.utils
 class NFUtils {
     def static ALLOWED_NF_PROCESS_DIRECTIVES = [
             'accelerator', 'afterScript', 'arch', 'beforeScript', 'cache', 'clusterOptions', 'conda', 'container',
-            'containerOptions', 'cpus', 'debug', 'disk', 'echo', 'errorStrategy', 'executor', /*'ext'*/ 'fair', 'label',
+            'containerOptions', 'cpus', 'debug', 'disk', 'echo', 'errorStrategy', 'executor', 'ext', 'fair', 'label',
             'machineType', 'maxSubmitAwait', 'maxErrors', 'maxForks', 'maxRetries', 'memory', 'module', 'penv', 'pod',
             'publishDir', 'queue', 'resourceLabels', 'scratch', 'shell', 'spack', 'stageInMode', 'stageOutMode', 'storeDir',
             'tag', 'time', 'template'
